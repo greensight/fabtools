755b38813ccd7e9c45709b2c34c6663015031548
Dmitry Krupnov <dmitry.krupnov@gmail.com>
2013-04-04 16:16:44 +0400

    django south migrations update

--- fabtools/require/django.py
+++ fabtools/require/django.py
@@ -9,6 +9,6 @@ def migrate_schema(use_sudo=False, **kw):
     func('python manage.py syncdb')
     for app in env.django['south_apps']:
         with settings(hide('warnings'), warn_only=True):
-            if func('python manage.py schemamigration %s --auto' % app).succeeded:
-                func('python manage.py migrate %s' % app)
+            func('python manage.py schemamigration %s --auto' % app)
+            func('python manage.py migrate %s' % app)
     func('python manage.py migrate --all')
